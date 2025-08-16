c904de04d7f2229e165587a883fecbd5cbe4c67c
singha111subhajit <singha111subhajit@gmail.com>
2025-08-16 19:15:27 +0530

    tst

--- MusicPlayerApp/app/src/main/java/com/example/musicplayer/config/ApiConfig.kt
+++ MusicPlayerApp/app/src/main/java/com/example/musicplayer/config/ApiConfig.kt
@@ -2,5 +2,5 @@ package com.example.musicplayer.config
 
 object ApiConfig {
     // Set this to your backend base URL, must end with '/'
-    const val BASE_URL: String = "https://your-backend.example.com/"
+    const val BASE_URL: String = "http://192.168.1.16:5600/"
 }
